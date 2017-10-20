c11a985fe477718d24bb07106916f1b322cecf2a
神楽坂雅詩 <cyscxchope@gmail.com>
2017-10-20 14:52:04 +0800

    加入运行路径,加入网页和SQL检测演示,加入日志清除

--- README.md
+++ README.md
@@ -3,31 +3,52 @@
 - 这个 sh 脚本可以让服务器自动监视几个页面，检测响应时间和返回信息，并在本地记录一段时间，以便检查问题。
 - 附带的 php 脚本可以将服务器近期的运行情况可视化返回。
 
-## 演示
+## 演示 Demo
 - [https://www.yoooooooooo.com/yashi/?page_id=5408](https://www.yoooooooooo.com/yashi/?page_id=5408)
 - [https://www.yoooooooooo.com/status](https://www.yoooooooooo.com/status)
+- 该程序在下面的环境中测试通过：
+  - Linux 3.10.0-693.2.2.el7.x86_64 (CentOS 7), bash 4.2.46, Nginx 1.12.1
 
 ## 与上一版本的区别
 - 上一版本使用 Java 制作，本版本仅仅是个 sh 脚本，通过调用 curl 等常用命令完成，不用任何多余的东西，节约资源。
 - 功能精简很多，修改设置由配置文件改为直接修改脚本即可。
 - 上一版本已移动到“v1java”分支。
 
-# 安装
-- 运行 sh 需要确保有 find, cat, curl, crontab 命令，访问 PHP 需要网页服务器和 HTML5 浏览器。
+# 安装 Install
+- 安装前要求：
+  - 确保有 find, cat, curl 命令。
+  - crontab 服务正常运行。
+  - 如需使用PHP图标页，要有网页服务。
+  - 如需使用PHP图标页，要有 HTML5 浏览器。
 - 将本脚本所有相关文件放在一个单独的、有执行权限的文件夹中。
-- 编辑 `status.sh` ：
+- 编辑 `clearlog.sh` ,以后运行它可以清空日志：
+  - 将第一条 `cd` 命令后面的地址改为 `status.sh` 所在的文件夹。
   - 将 `infofile` 路径设置在你的网页访问文件夹中，不要修改文件名。
-  - 将 php 文件复制到这个文件夹中。
+- 将 `index.php` 文件复制到 `infofile` 同一个文件夹中。
+- 编辑 `status.sh` ,以后定时运行它可以生成新日志：
+  - 将第一条 `cd` 命令后面的地址改为 `status.sh` 所在的文件夹。
+  - 将 `infofile` 路径设置在你的网页访问文件夹中，不要修改文件名。
   - 在 #检查网络 行的下面，修改和增加你要监视的页面，每行一个：
     - `sh statusl.sh "$logfile" "英文代码|显示名子" "访问网址" "预期得到的内容"`
     - 例如： `sh statusl.sh "$logfile" "web1|我的网站" "http://yashi.moe/test.txt" "ok."`
     - 请删除原来脚本中 `sh statusl.sh` 开头的行。
+    - 此处写的测试顺序应与 `index.php` 访问时想要呈现的图标顺序相反。
+- 准备好要各测试目标的返回值，确保可以 GET 请求网页。这里有两个演示：
+  - `checkpoint.php`
+    - 拷贝到要测试目标的网页服务器中，访问时提供source。
+    - 设置里面的返回信息。
+  - `checkpointsql.php`
+    - 拷贝到要测试目标的网页服务器中，访问时提供source。
+    - 设置里面的返回信息。
+    - 设置里面的数据库信息。
+- 运行 `status.sh` ，访问 `yashisitestatus.txt` 或 `index.php` 查看输出，检查是否有问题。
 - 运行 `crontab -e` ，将 `status.sh` 加入。
   - 脚本不会常驻后台，在 `crontab` 中设定计划任务，指定多久运行一次即可。
   - 下面的例子是每半个小时运行一次，请将后面的路径改为自己的：
   - `*/30 * * * * /bin/bash /mnt/d/yashisitestatus/status.sh`
+- 运行 `sh clearlog.sh` 进行一下初始清空。
 
-# 使用
+# 使用 Use
 - 在工作一段时间后，可以打开 `yashisitestatus.txt` 文件查看日志。
 - 挂在网页服务上，访问和它在一起的 php 文件，可以呈现运行状态图表。
   - PHP 可用参数 (GET方式提交，例如 `/index.php?zoom-h=100&zoom-w=10` )：
@@ -35,5 +56,5 @@
     - `zoom-h=500` ：图表单位柱形的高度容量，默认值 500，可以改为 10000 - 1 之间。
     - `zoom-w=10` ：图标单位柱形的宽度像素，默认值 10，可以改为 1 - 1024 之间。
 
-# 协议
+# 协议 License
 MIT.
--- status.sh
+++ status.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #NyarukoStatus/神楽坂雅诗
+cd "/mnt/www/yashisitestatus" #脚本所在的文件夹
+infofile="/mnt/www/yashisitestatus.txt" #index.php 所在的文件夹
 logfile="wstat`date +%Y%m%d%H%M%S`.log"
-infofile="/mnt/www/yashisitestatus.txt"
-#检查网络
-sh statusl.sh "$logfile" "yoooooooooo|www.yoooooooooo.com" "http://www.yoooooooooo.com/checkpoint.php?yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiBJ1\",\"host\":\"www.yoooooooooo.com\"}"
-sh statusl.sh "$logfile" "yoooooooooossl|www.yoooooooooo.com (SSL)" "https://www.yoooooooooo.com/checkpoint.php?yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiBJ1\",\"host\":\"www.yoooooooooo.com\"}"
+#检查网络，和PHP显示顺序相反 // sh statusl.sh "$logfile" "英文代码|显示名子" "访问网址" "预期得到的内容"
+sh statusl.sh "$logfile" "yoooooooooo|www.yoooooooooo.com" "http://www.yoooooooooo.com/checkpoint.php?source=yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiBJ1\",\"host\":\"www.yoooooooooo.com\",\"source\":\"yashiserverstatus\"}"
 #
-sh statusl.sh "$logfile" "uuumoe|uuu.moe" "http://uuu.moe/checkpoint.php?yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiHK1\",\"host\":\"uuu.moe\"}"
+sh statusl.sh "$logfile" "yoooooooooossl|www.yoooooooooo.com (SSL)" "https://www.yoooooooooo.com/checkpoint.php?source=yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiBJ1\",\"host\":\"www.yoooooooooo.com\",\"source\":\"yashiserverstatus\"}"
 #
-sh statusl.sh "$logfile" "yashimoe|yashi.moe" "http://yashi.moe/checkpoint.php?yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiHK1\",\"host\":\"yashi.moe\"}"
+sh statusl.sh "$logfile" "uuumoe|uuu.moe" "http://uuu.moe/checkpoint.php?source=yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiHK1\",\"host\":\"uuu.moe\",\"source\":\"yashiserverstatus\"}"
 #
-find ./ -mtime +3 -name "wstat*.log" -exec rm -rf {} \;
+sh statusl.sh "$logfile" "yashimoe|yashi.moe" "http://yashi.moe/checkpoint.php?source=yashiserverstatus" "{\"status\":0,\"msg\":\"OK\",\"server\":\"YashiHK1\",\"host\":\"yashi.moe\",\"source\":\"yashiserverstatus\"}"
+#日志整理 // +7:日志保留天数
+find ./ -mtime +7 -name "wstat*.log" -exec rm -rf {} \;
 echo -e "\c" > $infofile
 for nowfile in ./wstat*.log
 do
